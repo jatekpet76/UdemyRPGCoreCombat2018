8d50c1574ce9b5949984ab8327870a3da0942775
Godzzo <egodzzo@gmail.com>
2020-03-10 07:34:20 +0100

    change Editor 2019.3 and add Tresures and Fixing SassyJoe :)

---
+++ Assets/Scripts/TreasureController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureController : MonoBehaviour
+{
+    [SerializeField] Vector3 _rotate = new Vector3(10, 0, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(_rotate * Time.deltaTime);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-UdemyRPGCoreCombat2018
+# UdemyRPGCoreCombat2018
+Udemy RPGCoreCombat course - Unity3D 2018
