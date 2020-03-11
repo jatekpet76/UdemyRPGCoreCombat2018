7a2aafa26aa5c8523c6e595bc6864625a7965b97
Godzzo <egodzzo@gmail.com>
2020-03-11 07:10:36 +0100

    Setting up run animation, works when Sassy not turning :) and handle near destination.

--- Assets/Scripts/SassyJoeController.cs
+++ Assets/Scripts/SassyJoeController.cs
@@ -7,6 +7,8 @@ public class SassyJoeController : MonoBehaviour
 {
     [SerializeField] Transform[] _waitpoints;
     int _waitPoint = 0;
+
+    private Animator _animator;
     NavMeshAgent _navAgent;
     float _nextTime = 0;
 
@@ -26,6 +28,8 @@ public class SassyJoeController : MonoBehaviour
             }
         }
 
+        _animator = GetComponent<Animator>();
+
         _navAgent = GetComponent<NavMeshAgent>();
         _navAgent.speed = 4;
     }
@@ -33,13 +37,44 @@ public class SassyJoeController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        HandleWaitPoint();
+
+
+        // For Gravity RigidBody has velocity but it should Kinematic?  (_navAgent.velocity.x > 0 || _navAgent.velocity.z > 0)
+        // Sometimes working but not every time :(  !_navAgent.isStopped
+        // !!! When it rotating then it could not play the HumanoidRun animation. Why?
+        _animator.SetBool("isRunning", !_navAgent.isStopped);
+    }
+
+    private void HandleWaitPoint()
+    {
         if (_nextTime < Time.time)
         {
-            _nextTime = Time.time + Random.Range(4, 8);
-
-            int choosen = Random.Range(0, _waitpoints.Length - 1);
-
-            _navAgent.destination = _waitpoints[choosen].position;
+            NextWaitPoint();
         }
+        else
+        {
+            WaitPointTooClose();
+        }
+    }
+
+    private void WaitPointTooClose()
+    {
+        var distance = Vector3.Distance(transform.position, _navAgent.destination);
+
+        if (distance < 3 && !_navAgent.isStopped)
+        {
+            _navAgent.isStopped = true;
+        }
+    }
+
+    private void NextWaitPoint()
+    {
+        _nextTime = Time.time + Random.Range(4, 8);
+
+        int choosen = Random.Range(0, _waitpoints.Length - 1);
+
+        _navAgent.destination = _waitpoints[choosen].position;
+        _navAgent.isStopped = false;
     }
 }
